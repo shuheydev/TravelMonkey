e19b2c637d7d890c287a02cf446e203d6617d203
Shuhey <shuhey.dev@gmail.com>
2020-04-29 19:30:48 +0900

    Added ChatPage and ChatPageViewModel

---
+++ src/TravelMonkey/ViewModels/ChatPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelMonkey.ViewModels
+{
+    public class ChatPageViewModel:BaseViewModel 
+    {
+        private string _bindingTest="This is Binding test.";
+        public string BindingTest
+        {
+            get => _bindingTest;
+            set => Set(ref _bindingTest, value);
+        }
+    }
+}
---
+++ src/TravelMonkey/Views/ChatPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelMonkey.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelMonkey.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatPage : ContentPage
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new ChatPageViewModel();
+        }
+    }
+}
