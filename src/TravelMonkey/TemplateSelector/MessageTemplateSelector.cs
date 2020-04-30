c6d95d3e04497ee4ba8fa89c3d056cf2065ae3e0
Shuhey <shuhey.dev@gmail.com>
2020-04-30 11:50:44 +0900

    Implement ChatPage.

---
+++ src/TravelMonkey/TemplateSelector/MessageTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelMonkey.Models;
+using Xamarin.Forms;
+
+namespace TravelMonkey.TemplateSelector
+{
+    public class MessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MyMessageTemplate { get; set; }
+        public DataTemplate OthersMessageTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((Message)item).SenderType == SenderType.Me ? MyMessageTemplate : OthersMessageTemplate;
+        }
+    }
+}
